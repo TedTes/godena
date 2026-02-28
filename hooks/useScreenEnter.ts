3d3ef1ad3e14c73b8715b7f0ae84444dd94b1041
Tedros <tedtfu@gmail.com>
2026-02-27 20:34:46 -0500

    bug fixs

--- hooks/useScreenEnter.ts
+++ hooks/useScreenEnter.ts
@@ -3,17 +3,17 @@ import { Animated, Easing } from 'react-native';
 import { useFocusEffect } from '@react-navigation/native';
 
 export function useScreenEnter() {
-  const opacity = useRef(new Animated.Value(0)).current;
+  const opacity = useRef(new Animated.Value(1)).current;
   const translateY = useRef(new Animated.Value(10)).current;
 
   useFocusEffect(
     useCallback(() => {
-      opacity.setValue(0);
+      opacity.setValue(1);
       translateY.setValue(10);
       Animated.parallel([
         Animated.timing(opacity, {
           toValue: 1,
-          duration: 220,
+          duration: 120,
           easing: Easing.out(Easing.cubic),
           useNativeDriver: true,
         }),
